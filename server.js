b02124c90af2c695653875a7340b11c2b31ec111
Jason Asahara <jasahara@yahoo.com>
2016-01-27 22:36:38 -0800

    did name validations

--- server.js
+++ server.js
@@ -22,6 +22,19 @@ adminRouter.use(function(req, res, next) {
   next();
 });
 
+// route middleware to validate :name
+adminRouter.param('name', function(req, res, next, name) {
+  // do validation on name here
+  // blah blah validation
+  // log something so we know its working
+  console.log('doing name validations on ' + name);
+
+  // once validation is done save the new item in req
+  req.name = name;
+  // go to the next thing
+  next();
+});
+
 // admin main page.  the dashboard (http://localhost:1337/admin)
 adminRouter.get('/', function(req, res) {
   res.send('I am the dashboard!');
@@ -42,8 +55,13 @@ adminRouter.get('/users/:name', function(req, res) {
   res.send('hello ' + req.params.name + '!');
 })
 
+// route with parameters (http://localhost:1337/hello/:name)
+adminRouter.get('/hello/:name', function(req, res) {
+  res.send('hello ' + req.name + '!')
+});
+
 // apply the routes to our application
-app.use('/admin', adminRouter);
+app.use('/', adminRouter);
 
 // start the server
 app.listen(1337);
